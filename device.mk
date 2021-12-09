02be5b6212d77064578ad28b6cd9aa23ce0e434d
Dil3mm4 <dil3mm4.dev@gmail.com>
2021-12-09 15:31:56 +0100

    surya: rro_overlays: Add notch overlay killer
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -355,7 +355,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
 PRODUCT_PACKAGES += \
-    KarnaPowerProfile
+    KarnaPowerProfile \
+    NotchBarKiller
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
