f67214158707de1a4ecfd2e7265744f7269140d5
dlwlrma123 <alexfinhart@gmail.com>
2022-01-06 19:24:07 +0800

    surya/Boardconfig: disable LMKD stats * this is only helpful when kernels use memcg/psi but most kernels here on surya ships SLMK making LMKD stats useless and an additional overhead.
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,7 +131,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # LMKD
-TARGET_LMKD_STATS_LOG := true
+TARGET_LMKD_STATS_LOG := false
 
 # Media
 TARGET_USES_ION := true
